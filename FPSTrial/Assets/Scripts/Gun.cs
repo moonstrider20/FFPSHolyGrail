c1993662ee705b7a6a80ad63e59a47097ff580c5
moonstrider20 <46873254+moonstrider20@users.noreply.github.com>
2021-02-17 15:19:06 -0500

    BasicMechanics
    Building the basic mechanics of the game. So far in FF camera, movement, and gun

---
+++ FPSTrial/Assets/Scripts/Gun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 50f;
+
+    public Camera fpsCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
