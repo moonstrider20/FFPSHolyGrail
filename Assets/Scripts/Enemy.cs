fbbb5eed8bc257096edf1627afc6017bdaad9196
VTaipan <kwilson269@gmail.com>
2021-02-23 13:20:13 -0500

    Added menus

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask Ground, Player;
+
+    public float health;
+    public float startHealth;
+
+    public int stage;
+
+    public float newSpeed;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    //public GameObject projectile;
+
+    //States
+    public float sightRange;
+    public float attackRangeMelee;
+    public float attackRangeRange;
+    public bool playerInSightRange;
+    public bool playerInAttackRangeMelee;
+    public bool playerInAttackRangeRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        stage = 1;
+        startHealth = health;
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
+        playerInAttackRangeMelee = Physics.CheckSphere(transform.position, attackRangeMelee, Player);
+        playerInAttackRangeRange = Physics.CheckSphere(transform.position, attackRangeRange, Player);
+
+        if (!playerInSightRange && !playerInAttackRangeMelee && !playerInAttackRangeRange)
+        {
+            Patroling();
+        }
+
+        if (playerInSightRange && !playerInAttackRangeMelee && !playerInAttackRangeRange)
+        {
+            ChasePlayer();
+        }
+
+        if (playerInSightRange && !playerInAttackRangeMelee && playerInAttackRangeRange)
+        {
+            AttackPlayer();
+            ChasePlayer();
+        }
+
+        if (playerInSightRange && playerInAttackRangeMelee)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, Ground))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move....NOTWORKING-_-
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+            if (stage == 1)
+            {
+                if (playerInAttackRangeMelee)
+                {
+                    Debug.Log("Stage 1 --- Kick");
+                }
+
+                else
+                {
+                    Debug.Log("Stage 1 --- Gun");
+                }
+                //Debug.Log("Hit in stage 1");
+            }
+
+            else if (stage == 2)
+            {
+                if (playerInAttackRangeMelee)
+                {
+                    Debug.Log("STAGE 2 --- Axe");
+                }
+
+                else
+                {
+                    Debug.Log("STAGE 2 --- Gun");
+                }
+                //Debug.Log("HIT STAGE 2");
+            }
+
+            else
+            {
+                if (playerInAttackRangeMelee)
+                {
+                    Debug.Log("sTaGE 3 --- Axe");
+                }
+
+                else
+                {
+                    Debug.Log("sTaGE 3 --- Throwing Axe");
+                }
+                //Debug.Log("sTaGE 3!!");
+            }
+            //Debug.Log("IM HIT IM HIT!");
+            //Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            //rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= ((startHealth / 3) * 2))
+        {
+            stage = 2;
+        }
+
+        if (health <= (startHealth / 3))
+        {
+            stage = 3;
+            agent.speed = newSpeed;
+        }
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    /*private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }*/
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,113 @@
+﻿//Amorina Tabera
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour, PlayerControls.IPlayerActions
+{
+    public float speed;                     //Speed of Player
+    public PlayerControls controls;         //Gets Input System Controls from script called PlayerControls
+    public Vector2 motion;                  //Vector for movement of Player
+    public Vector2 mouseInput;              //Vector for looking around for Player
+
+    //private Rigidbody rb;                   //rigidbody of the Player
+    [SerializeField] private CharacterController controller = null;
+
+    public float sensitivity;              //Mouse sensitivity
+    public float MaxLookUpAngle;            //How high Player can look
+    public float MinLookUpAngle;            //How low Player can look
+
+    public float rotX = 0, rotY = 0;        //Rotations for camera
+
+    public Transform MainCamera;            //To hold the FFCamera
+
+    public Gun gun;                         //Get Gun script
+
+    Vector3 movement;
+    bool button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();     //Get the rigidbody component of the player
+        controller = GetComponent<CharacterController>();   //Get the Character Controller component of the player
+        Cursor.lockState = CursorLockMode.Locked;   //Keep cursor in the center, not wandering
+        Cursor.visible = false;                     //Make cursor invisible 
+    }
+
+    //To enable the Input System contorls for "Player"
+    public void OnEnable()
+    {
+        //safety thing
+        if (controls == null)
+        {
+            controls = new PlayerControls();
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    //To disable the Input System controls for "Player"
+    public void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    //Inputsystem.inputActioins.Player.Move.triggered
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        motion = context.ReadValue<Vector2>();                      //Get the values of the directions Player is moving (arrows, WASD, ect)
+    }
+
+    //Inputsystem.inputActions.Player.Look.triggered
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseInput = context.ReadValue<Vector2>();                  //read values of the mouse(Look) motion
+    }
+
+    //Inputsytem.inputActions.Player.Gun.triggered
+    public void OnGun(InputAction.CallbackContext context)
+    {
+        button = context.ReadValueAsButton();                       //Redundant, trying to fix double press problem
+        if (button)
+        {
+            //gun.Shoot();                                            //Call method Shoot from script Gun
+            gun.MyInput();
+        }
+    }
+
+    //Inputsystem.inputActions.Player.Quit.triggered
+    public void OnQuit(InputAction.CallbackContext context)
+    {
+        Application.Quit();                                         // Quit the game
+    }
+
+    void Update()
+    {
+        Move();
+        Turn();
+        //Shooting();
+        //Quitting();
+
+    }
+
+    //Method to move (should be in OnMovement but didn't work for some reason
+    private void Move()
+    {
+        movement = transform.right * motion.x + transform.forward * motion.y; // GLOBAL[new Vector3(motion.x, 0.0f, motion.y);] <- that but relative to the direction of the camera
+        controller.Move(movement * Time.deltaTime * speed);                   //Move the Player
+    }
+
+    //Method to look around (should be in OnLook, but left here since the movement didn't work)
+    private void Turn()
+    {
+        rotX += mouseInput.x * sensitivity * Time.deltaTime;                            //Get horizontal input
+        rotY += mouseInput.y * sensitivity * Time.deltaTime;                            //Get vertical input
+        rotY = Mathf.Clamp(rotY, MinLookUpAngle, MaxLookUpAngle);                       //Restrict how far up and down Player can look
+
+        MainCamera.transform.localRotation = Quaternion.Euler(-rotY, mouseInput.x, 0f); //Move camera with mouse
+        transform.Rotate(Vector3.up * mouseInput.x);                                    //Move Player with camera
+    }
+}
